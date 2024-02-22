4807b14fc295835c97b3f77807d692bb12b0d524
mandrindra12 <mandrindraantonnio@gmail.com>
2024-02-22 13:48:48 +0300

    first test for triangularisation

--- gauss.c
+++ gauss.c
@@ -1,9 +1,26 @@
 #include <stdlib.h>
 
-void triangularisation() {
-
+void triangularisation(int **first, int *second, int dim) {
+    for(int k = 0; k < dim; k++) {
+        for(int i = k+1; i < dim; i++) {
+            for(int j = k+1; j < dim; j++) {
+                first[i][j] = (first[i][j]-first[i][k]) / (first[k][k]*first[k][j]);
+            }
+            second[i] = (b[i]-first[i][k])/(first[k][k]*b[k]);
+            first[i][k] = 0;
+        }
+    }
 }
 
 void resolution() {
-    
+
+}
+
+void print(int **matrix, int dim) {
+    for(int i = 0; i < dim ; i++) {
+        for(int j = 0; j < dim; j++) {
+            printf("%d\t", matrix[i][j]);
+        }
+        printf("\n");
+    } 
 }
--- gauss.h
+++ gauss.h
@@ -1,11 +1,8 @@
 #ifndef GAUSS_H
 #define GAUSS_H
 
-void triangularisation(int *coeffs) {
-
-}
-void resolution() {
-
-}
+void triangularisation(int **first_members, int *second_member, int dim);
+void resolution();
+void print(int **matrix, int dim);
 
 #endif
