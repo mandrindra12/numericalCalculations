1e9346fe054085da889fb4b515aff1639801ba43
mandrindra12 <mandrindraantonnio@gmail.com>
2024-02-22 15:33:40 +0300

    resolution method added

--- gauss.h
+++ gauss.h
@@ -2,7 +2,8 @@
 #define GAUSS_H
 
 void triangularisation(float **first_members, float *second_member, int dim);
-void resolution();
+float *resolution(float **first, float *second,  int dim);
 void print(float **matrix, int dim);
+void p(float *f);
 
 #endif
