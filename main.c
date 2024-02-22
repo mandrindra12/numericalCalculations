9249c5c334dee33bbf12e13d8dc2a0c316f25c45
mandrindra12 <mandrindraantonnio@gmail.com>
2024-02-22 15:33:21 +0300

    test for resolution

--- main.c
+++ main.c
@@ -12,8 +12,9 @@ int main() {
     a[0][0] = 4; a[0][1] = 8; a[0][2] = 12;
     a[1][0] = 3; a[1][1] = 8; a[1][2] = 13;
     a[2][0] = 2; a[2][1] = 9; a[2][2] = 18;
-
-    triangularisation(a, b, 3);
+    float *x = resolution(a, b, 3);
     print(a, 3);
+    p(x);
+    p(b);
     return 0;
 }
